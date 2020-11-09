e7873c31a8fa386884415a2f2d7c931d5b305734
yiwen li <lix892314@gmail.com>
2020-11-08 18:44:45 -0700

    module 8

---
+++ mod8-solution/js/app.js
@@ -0,0 +1,79 @@
+(function () {
+'use strict';
+
+angular.module('NarrowItDownApp', [])
+       .controller('NarrowItDownController', NarrowItDownController)
+       .service('MenuSearchService', MenuSearchService)
+       .directive('foundItems', FoundItemsDirective);
+
+function FoundItemsDirective() {
+  var ddo = {
+    templateUrl: 'menuItemList.html',
+    scope: {
+      found: '<',
+      onRemove: '&'
+    },
+  };
+
+  return ddo;
+}
+
+MenuSearchService.$inject = ['$http'];
+function MenuSearchService($http){
+   var service = this;
+
+   service.getPromise = function(){
+      var promise = $http({
+        method: "GET",
+        url: "https://davids-restaurant.herokuapp.com/menu_items.json"
+      });
+      return promise;     
+   };
+
+   service.getMatchedMenuItems = function(items,searchTerm){
+     var foundItems = [];
+     for (var i=0; i<items.length; i++) {
+       var description = items[i].description;
+       if (description.indexOf(searchTerm) !== -1){
+         foundItems.push(items[i]);
+       }
+     }
+     return foundItems;
+   };
+
+}
+
+
+NarrowItDownController.$inject = ['MenuSearchService'];
+
+function NarrowItDownController(MenuSearchService){
+  var ctrl = this;
+  ctrl.found = [];
+
+  ctrl.getMatchedMenuItems = function(){
+    var promise = MenuSearchService.getPromise();
+    promise.then(function (response) {
+        var returnedResult = response.data;
+        var allItems = returnedResult.menu_items;
+        console.log(ctrl.search);
+        ctrl.found = MenuSearchService.getMatchedMenuItems(allItems,ctrl.search);
+        if (ctrl.found.length == 0) {
+          ctrl.message = "Nothing Found.";
+        }
+        else {
+          ctrl.message = "";
+        }
+      })
+      .catch(function (error) {
+        console.log("Error message: " + error);
+      });
+    
+  };//end of getMatchedMenuItems
+
+  ctrl.removeItem = function(index){
+    ctrl.found.splice(index, 1);
+  };
+  
+}//end of controller
+
+})();
